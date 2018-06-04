908796bb5fb4d010b4dacd31db849457fd312c08
gillenha <gillen08@gmail.com>
2018-06-04 14:22:06 -0400

    added graphiql api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
 const Painting = require('./models/Painting');
+const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+const schema = require('./graphql/schema');
 
 const server = hapi.server({
 	port: 4000,
@@ -14,6 +16,33 @@ mongoose.connection.once('open', () => {
 });
 
 const init = async () => {
+
+	await server.register({
+		plugin: graphiqlHapi,
+		options: {
+			path: '/graphiql',
+			graphiqlOptions: {
+				endpointURL: '/graphql'
+			},
+			route: {
+				cors: true
+			}
+		}
+	});
+
+	await server.register({
+		plugin: graphqlHapi,
+		options: {
+			path: '/graphql',
+			graphqlOptions: {
+				schema
+			},
+			route: {
+				cors: true
+			}
+		}
+	});
+
 	server.route([
 	{
 		method: 'GET',
